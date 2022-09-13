d39a3add1308abb5d2a0770250839e1c9235eaa1
TheBlackCookie <76895655+TheBlackCookie@users.noreply.github.com>
2022-09-14 00:02:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests, random, time, logging, os, ctypes
 os.system("")
+os.system("cls")
 logging.basicConfig(
     level=logging.INFO,
     format="\x1b[38;5;63m[\x1b[0m%(asctime)s\x1b[38;5;63m]\x1b[0m %(message)s",
