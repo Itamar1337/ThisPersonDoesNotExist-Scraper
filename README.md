83d56f6874579741d38f9d83c4299cf678815dd8
TheBlackCookie <76895655+TheBlackCookie@users.noreply.github.com>
2022-09-14 00:00:51 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+## 🚀・Configure ThisPersonDoesNotExist-Scraper
+
+You'll need [Git](https://git-scm.com) to start off!
+### 1. From your command line, clone and configure the checker:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/TheBlackCookie/ThisPersonDoesNotExist-Scraper.git
+$ python main.py
+```
+
+## 🤝・Contributing
+
+Any ideas on how to improve the checker? Or just think you got something you want to see being added? [Open a new issue](https://github.com/TheBlackCookie/ThisPersonDoesNotExist-Scraper/issues)!
+
+Found a bug? please please please [Open a new issue](https://github.com/TheBlackCookie/ThisPersonDoesNotExist-Scraper/issues) and tell me about it so I can fix it asap
+
+
+[I know its Very slow, using threads or making it faster will make it scrape the same photo twice.]
---
+++ main.py
@@ -0,0 +1,23 @@
+import requests, random, time, logging, os,
+os.system("")
+logging.basicConfig(
+    level=logging.INFO,
+    format="\x1b[38;5;63m[\x1b[0m%(asctime)s\x1b[38;5;63m]\x1b[0m %(message)s",
+    datefmt="%H:%M:%S"
+)
+Valid = 0
+def UpdateTitle(): 
+    ctypes.windll.kernel32.SetConsoleTitleW("[ThisPersonDoesNotExist Scraper] | Valid: %s" % (Valid))
+if not os.path.exists('Images'): os.makedirs('Images')
+while True:
+    time.sleep (1)
+    response = requests.get ("https://thispersondoesnotexist.com/image")
+    if response.status_code == 200:
+        Valid += 1
+        logging.critical ("Successfully Scraped Image")
+        UpdateTitle()
+    else:
+        logging.critical ("Failed to Scrape Image")
+    chars = ("abcdefghijklmnopqrstuvwxyz")
+    filename = "".join(random.choice(chars) for x in range(4))
+    with open(f'Images/{filename}.{"png"}', 'wb') as f: f.write(response.content)
